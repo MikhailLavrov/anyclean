aa1f07f4b9066e32745c98d600402429b2b90fa8
Mikhail Lavrov <lavrov.fd@gmail.com>
2024-10-23 21:44:34 +0300

    price descr

--- src/components/Price/Price.jsx
+++ src/components/Price/Price.jsx
@@ -1,25 +1,88 @@
 import c from './Price.module.css';
+import { Popover } from 'antd';
 
 const mainPriceData = [
-  {title: 'Поддерживающая уборка', price: '280'},
-  {title: 'Генеральная уборка', price: '300'},
-  {title: 'Уборка после ремонта', price: '450'},
+  {
+    title: 'Поддерживающая уборка',
+    price: '280',
+    description: `Протирание подоконников и поверхностей мебели
+    Протирание всех стеклянных и зеркальных поверхностей
+    Чистка ковровых покрытий 
+    Мытье  полов
+    Вынос  мусора 
+    Уборка  санузла`,
+  },
+  {
+    title: 'Генеральная уборка', 
+    price: '300',
+    description: `Весь перечень « Поддерживающей»   + дополнительно:
+
+    Обеспыливание люстр и светильников
+    Мытьё труб и батарей отопления
+    Обеспыливание бытовой и  орг. техники
+    Очищение внешних поверхностей всей кухонной техники
+    Мытье  сантехники
+    Протирка  смесителей`,
+  },
+  {
+    title: 'Уборка после ремонта', 
+    price: '450',
+    description: `Обеспыливание стен,  дверей и пола
+    Мытье стен,  дверей, плинтусов, полов
+    Мытье подоконников и батарей отопления
+    Мытьё зеркальных и стеклянных поверхностей 
+    Вынос мусора до 10 кг`,
+  },
 ];
 
 const addPriceData = [
-  {title: 'Химчистка мебели', price: '2000'},
-  {title: 'Погладить вещи', price: '2500'},
-  {title: 'Мойка окна', price: '1500'},
-  {title: 'Мойка люстры', price: '750'},
-  {title: 'Мойка холодильника', price: '950'},
-  {title: 'Мойка духового шкафа', price: '750'},
-  {title: 'Мойка микроволновой печи', price: '650'},
+  {
+    title: 'Химчистка мебели', 
+    price: '2000'
+  },
+  {
+    title: 'Погладить вещи', 
+    price: '2500'
+  },
+  {
+    title: 'Мойка окна', 
+    price: '1500'
+  },
+  {
+    title: 'Мойка люстры', 
+    price: '750'
+  },
+  {
+    title: 'Мойка холодильника', 
+    price: '950'
+  },
+  {
+    title: 'Мойка духового шкафа', 
+    price: '750'
+  },
+  {
+    title: 'Мойка микроволновой печи', 
+    price: '650'
+  },
 ];
 
 export const PriceComponent = () => {
   const mainPriceItems = mainPriceData.map((item, i) => (
     <li className={c.price__item} key={i}>
-      <p className={c.price__itemTitle}>{item.title}</p>
+      <p className={c.price__itemTitle}>
+        {item.title}
+        {item.description && 
+          <Popover
+            className={c.popover}
+            content={item.description.split('\n').map((line, index) => (
+              <p key={index}>{line}</p>
+            ))}
+            title={item.title}
+          >
+            i
+          </Popover>
+        }
+      </p>
       <span className={c.price__dots}></span>
       <p className={c.price__itemPrice}>{item.price} р/м2</p>
     </li>
